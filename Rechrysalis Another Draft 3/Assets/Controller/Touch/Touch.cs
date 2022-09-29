be43027635bade47bc4efbc7c38f500c2b6dd6b0
tjjoris <tjjoris@gmail.com>
2022-09-29 13:06:17 -0400

    making touch

---
+++ Rechrysalis Another Draft 3/Assets/Controller/Touch/Touch.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rechrysalis.Controller
+{
+    [System.Serializable]
+    [CreateAssetMenu(fileName ="TouchInstance", menuName ="Controller/Touch/Touch")]
+    public class Touch : ScriptableObject
+    {
+        [SerializeField] private ClickInfo _clickInfo;
+        [SerializeField] private CheckRayCastSO _checkRayCast;
+        public void Initialize(GameObject _controllerGO)
+        {
+            _clickInfo.ControlledController = _controllerGO;
+            _checkRayCast.ClickInfo = _clickInfo;
+        }
+
+    }
+}
