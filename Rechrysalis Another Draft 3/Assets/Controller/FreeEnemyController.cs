7a3c7b2110687f1fbac24d0d105f79963ff4ec46
tjjoris <tjjoris@gmail.com>
2022-09-30 09:13:22 -0400

    further setting up fre enemey controller

--- Rechrysalis Another Draft 3/Assets/Controller/FreeEnemyController.cs
+++ Rechrysalis Another Draft 3/Assets/Controller/FreeEnemyController.cs
@@ -11,6 +11,8 @@ namespace Rechrysalis.Controller
         public List<GameObject> UnitsList {get{return _unitsList;} set{_unitsList = value;}}
         [SerializeField] private PlayerUnitsSO[] _unitListSOArray;
         public PlayerUnitsSO[] UnitListSoArray {get{return _unitListSOArray;}set{_unitListSOArray = value;}}
+        [SerializeField] private FreeUnitCompSO _freeEnemyComp;
+        public FreeUnitCompSO FreeUnitComp{get{return _freeEnemyComp;}set{_freeEnemyComp = value;}}
         
     }
 }
