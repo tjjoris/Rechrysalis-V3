aa659d47efaa21d781063f532f00776563c94894
tjjoris <tjjoris@gmail.com>
2022-10-04 15:14:40 -0400

    fixed clicking unit
    stop still works

--- Rechrysalis Another Draft 3/Assets/Controller/Raycast and Data/CheckRayCastSO.cs
+++ Rechrysalis Another Draft 3/Assets/Controller/Raycast and Data/CheckRayCastSO.cs
@@ -14,46 +14,46 @@ namespace Rechrysalis.Controller
         
         public void CheckRayCastDownFunction(Vector2 _mousePos, int _touchID)
         {
-                LayerMask _mask = ~LayerMask.GetMask("PlayerController");
-                _mask += LayerMask.GetMask("Unit");
-                RaycastHit2D hit = Physics2D.Raycast(_mousePos, Vector2.zero, _mask);
-                if (hit) 
+            LayerMask _mask = ~LayerMask.GetMask("PlayerController");
+            _mask += LayerMask.GetMask("Unit");
+            RaycastHit2D hit = Physics2D.Raycast(_mousePos, Vector2.zero, _mask);
+            if (hit) 
+            {
+                if (hit.collider.gameObject.layer == 6)
                 {
-                    if (hit.collider.gameObject.layer == 7)
+                    Debug.Log($"stop");
+                    _clickInfo.ControlledController.GetComponent<Mover>().IsStopped = true;
+                }
+                else if (hit.collider.gameObject.layer == 7)
+                {
+                    Debug.Log("unitHit");
+                    UnitManager _unitManager = hit.collider.gameObject.GetComponent<UnitManager>();
+                    if (_unitManager != null)
                     {
-                        Debug.Log($"stop");
-                        _clickInfo.ControlledController.GetComponent<Mover>().IsStopped = true;
+                        Debug.Log($"clicked unit");
                     }
-                    else if (hit.collider.gameObject.layer == 8)
+                    if ((_unitManager != null) && (_unitManager.ControllerIndex == 1))
                     {
-                        Debug.Log("unitHit");
-                        UnitManager _unitManager = hit.collider.gameObject.GetComponent<UnitManager>();
-                        if (_unitManager != null)
-                        {
-                            Debug.Log($"clicked unit");
-                        }
-                        if ((_unitManager != null) && (_unitManager.ControllerIndex == 1))
-                        {
-                            Debug.Log($"click enemy");
-                        }
+                        Debug.Log($"click enemy");
                     }
-                else if (false)
-                {
-                    //ring to rotate
                 }
-                else if (false)
-                {
-                    //menu clicked
-                }
-                else 
-                {//map clicked
-                    Debug.Log("map clicked " + _mousePos.ToString());
-                    _clickInfo.FingerIDMove = _touchID;
-                    Vector2 _direction = _clickInfo.ControlledController.transform.position;
-                    _direction = _mousePos - _direction;
-                    _clickInfo.ControlledController.GetComponent<Mover>().Direction = _direction;
-                    _clickInfo.ControlledController.GetComponent<Mover>().IsStopped = false;
-                }
+            }
+            else if (false)
+            {
+                //ring to rotate
+            }
+            else if (false)
+            {
+                //menu clicked
+            }
+            else 
+            {//map clicked
+                Debug.Log("map clicked " + _mousePos.ToString());
+                _clickInfo.FingerIDMove = _touchID;
+                Vector2 _direction = _clickInfo.ControlledController.transform.position;
+                _direction = _mousePos - _direction;
+                _clickInfo.ControlledController.GetComponent<Mover>().Direction = _direction;
+                _clickInfo.ControlledController.GetComponent<Mover>().IsStopped = false;
             }
         }
     }
