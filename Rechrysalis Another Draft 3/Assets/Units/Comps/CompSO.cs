e49b66895deb0106035da0ceaf3e4f6e2c7add2b
tjjoris <tjjoris@gmail.com>
2022-10-05 21:47:51 -0400

    added parent unit count, and child unit count to comp SO

--- Rechrysalis Another Draft 3/Assets/Units/Comps/CompSO.cs
+++ Rechrysalis Another Draft 3/Assets/Units/Comps/CompSO.cs
@@ -8,7 +8,11 @@ namespace Rechrysalis.Unit
     [CreateAssetMenu(fileName ="CompSO", menuName ="Comps/CompSO")]
     public class CompSO : ScriptableObject
     {
-        [SerializeField] private UnitStatsSO[] _unitSOArray;
+        [SerializeField] private UnitStatsSO[] _unitSOArray;        
         public UnitStatsSO[] UnitSOArray {get{return _unitSOArray;}}
+        [SerializeField] private int _parentUnitcount;
+        public int ParentUnitCount {get { return _parentUnitcount;}}
+        [SerializeField] private int _childUnitCount;
+        public int ChildUnitCount {get{return _childUnitCount;}}
     }
 }
