09ddccb0534819f42cea9a3ad8dc80c6243a34ab
tjjoris <tjjoris@gmail.com>
2022-10-08 10:34:38 -0400

    saving units from active unit in parent unit to active player units SO

--- Rechrysalis Another Draft 3/Assets/Units/Controller Units/ParentUnitManager.cs
+++ Rechrysalis Another Draft 3/Assets/Units/Controller Units/ParentUnitManager.cs
@@ -9,6 +9,7 @@ namespace Rechrysalis.Unit
         [SerializeField] private int _controllerIndex;
         [SerializeField] private GameObject[] _subUnits;
         public GameObject[] SubUnits {get {return _subUnits;}set {_subUnits = value;}}
+        private PlayerUnitsSO _theseUnits;
 
         private bool _isStopped;
         public bool IsStopped 
@@ -28,16 +29,25 @@ namespace Rechrysalis.Unit
         }
         public void ActivateUnit(int _unitIndex)
         {
-            for (int i=0; i<_subUnits.Length; i++)
+            for (int _indexInSubUnits=0; _indexInSubUnits<_subUnits.Length; _indexInSubUnits++)
             {
-                if (i == _unitIndex)  
+                if (_indexInSubUnits == _unitIndex)  
                 {
                     _subUnits[_unitIndex].SetActive(true);
+                    if (!_theseUnits.ActiveUnits.Contains(_subUnits[_indexInSubUnits]))
+                    {
+                        _theseUnits.ActiveUnits.Add(_subUnits[_unitIndex]);
+                    }
                 }
                 else 
                 {
-                    _subUnits[i].SetActive(false);
-                }
+                    _subUnits[_indexInSubUnits].SetActive(false);   
+                    if (_theseUnits.ActiveUnits.Contains(_subUnits[_indexInSubUnits]))
+                    {
+                        int _indexInActiveUnits = _theseUnits.ActiveUnits.IndexOf(_subUnits[_indexInSubUnits]);
+                        _theseUnits.ActiveUnits.Remove(_theseUnits.ActiveUnits[_indexInActiveUnits]);
+                    }                                     
+                }             
             }
         }
     }
