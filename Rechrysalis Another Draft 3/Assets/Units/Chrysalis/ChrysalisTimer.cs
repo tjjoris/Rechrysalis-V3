6193f7256c6b04ff914a2951371bfcb9770d9e9f
tjjoris <tjjoris@gmail.com>
2022-10-11 15:29:58 -0400

    setting up chrysalis timer

--- Rechrysalis Another Draft 3/Assets/Units/Chrysalis/ChrysalisTimer.cs
+++ Rechrysalis Another Draft 3/Assets/Units/Chrysalis/ChrysalisTimer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Rechrysalis.Unit
 {
@@ -8,10 +9,32 @@ namespace Rechrysalis.Unit
     {
         private float _timerMax;
         private float _timerCurrent;
+        private  int _nextUnitBuilding;
+        private int _subUnitCount;
+        public Action<int> _startUnit;
 
         public void Initialize (float _timerMax)
         {
-
+            this._timerMax = _timerMax;
+        }
+        public void StartThisChrysalis()
+        {
+            _timerCurrent = 0;            
+        }
+        public void Tick (float _timeAmount)
+        {
+            _timerCurrent += _timeAmount;
+            if (_timerCurrent >= _timerMax)
+            {
+                _startUnit?.Invoke(_nextUnitBuilding);
+            }
+        }
+        public void SetNextUnit(int _nextUnitBuilding)
+        {
+            if (_nextUnitBuilding <= _subUnitCount)
+            {
+                this._nextUnitBuilding = _nextUnitBuilding;
+            }
         }
     }
 }
