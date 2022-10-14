1da35bcc65fccf8d4a5dc4379524cd6043e1cfdb
tjjoris <tjjoris@gmail.com>
2022-10-13 21:15:01 -0400

    fixed unit ring angle

--- Rechrysalis Another Draft 3/Assets/Controller/RechrysalisController/RechrysalisControllerInitialize.cs
+++ Rechrysalis Another Draft 3/Assets/Controller/RechrysalisController/RechrysalisControllerInitialize.cs
@@ -29,7 +29,7 @@ namespace Rechrysalis.Controller
             // foreach (GameObject _unit in _parentUnits)
             for (int _parentUnitIndex = 0; _parentUnitIndex < _unitComp.ParentUnitCount; _parentUnitIndex++)
             {       
-                float _radToOffset = Mathf.Deg2Rad * (((360f / 3f) * _parentUnitIndex) + _unitRingManager.UnitRingAngle);  
+                float _radToOffset = Mathf.Deg2Rad * (((360f / 3f) * _parentUnitIndex) + _unitRingAngle);  
                 Vector3 _unitOffset = new Vector3 (Mathf.Cos(_radToOffset) * _ringDistFromCentre, Mathf.Sin(_radToOffset) * _ringDistFromCentre, 0f);
                 // Debug.Log($"radtooffset" + _radToOffset + "vector 3 " + _unitOffset);
                 GameObject go = Instantiate(_parentUnitPrefab, _unitRing.transform);
