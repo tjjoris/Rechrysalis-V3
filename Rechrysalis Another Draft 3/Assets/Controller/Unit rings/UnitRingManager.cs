a2a26e515b04b077da276a1ecc133b8b67f2b1ba
tjjoris <tjjoris@gmail.com>
2022-10-14 19:26:31 -0400

    making unit ring manager rotate functions

--- Rechrysalis Another Draft 3/Assets/Controller/Unit rings/UnitRingManager.cs
+++ Rechrysalis Another Draft 3/Assets/Controller/Unit rings/UnitRingManager.cs
@@ -8,6 +8,9 @@ namespace Rechrysalis.Controller
     {
         private float _unitRingAngle;
         public float UnitRingAngle {get{return _unitRingAngle;}}
+        private float _targetAngle;
+        private float _minAngleToMove = 1f;
+        private float _amountToRotate = 1f;
         [SerializeField] private float _unitDegreeWidth;
         public float UnitDegreeWidth {get{return _unitDegreeWidth;}}
         private float[] _unitDegreeWidthArray;
@@ -26,6 +29,26 @@ namespace Rechrysalis.Controller
                 _unitDegreeWidthArray[(_parentUnitIndex * 2) + 1] = ((360 / _numberOfParentUnits) * _parentUnitIndex) + _unitDegreeWidth;
             }
             }
+        }
+        public void SetTargetAngle(float _targetAngle)
+        {
+            this._targetAngle = _targetAngle;
+        }
+        public void Tick (float _timeAmount)
+        {
+            if (_targetAngle > _unitRingAngle + _minAngleToMove)
+            {
+                RotateRing(_timeAmount * _amountToRotate);
+            }
+            else if (_targetAngle < _unitRingAngle - _minAngleToMove)
+            {
+                RotateRing(-_timeAmount * _amountToRotate);
+            }
+        }
+        private void RotateRing (float _amountToRotate)
+        {
+            Vector3 _eluerAmngle = new Vector3 (0, 0, transform.eulerAngles.z + _amountToRotate);
+            transform.eulerAngles = _eluerAmngle;
         }
         public float[] GetUnitDegreeWidthArray()
         {
