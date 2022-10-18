ff1f1933fd58c23bdd2393d21cbd98e49ba7f760
tjjoris <tjjoris@gmail.com>
2022-10-18 12:33:05 -0400

    parent unit hatch effects adding hatch effects for units

--- Rechrysalis Another Draft 3/Assets/Units/Controller Units/ParentUnitHatchEffects.cs
+++ Rechrysalis Another Draft 3/Assets/Units/Controller Units/ParentUnitHatchEffects.cs
@@ -8,16 +8,24 @@ namespace Rechrysalis.Unit
     {
         private List<GameObject> _hatchEffects;
         private GameObject[] _subUnits;
-        private GameObject[] _subchrysalii;
+        private GameObject[] _subChrysalii;
         public void Initialize (GameObject[] _subUnits, GameObject[] _subchrysalii)
         {
             this._subUnits = _subUnits;
-            this._subchrysalii =_subchrysalii;
+            this._subChrysalii =_subchrysalii;
             _hatchEffects = new List<GameObject>();
         }
         public void AddHatchEffect(GameObject _hatchEffect)
         {
             _hatchEffects.Add(_hatchEffect);
+            foreach (GameObject _unit in _subUnits)
+            {
+                _unit.GetComponent<UnitManager>()?.AddHatchEffect(_hatchEffect);
+            }
+            foreach (GameObject _chrysalis in _subChrysalii)
+            {
+                _chrysalis.GetComponent<UnitManager>()?.AddHatchEffect(_hatchEffect);
+            }
         }
         public void RemoveHatchEffect(GameObject _hatchEffect)
         {
