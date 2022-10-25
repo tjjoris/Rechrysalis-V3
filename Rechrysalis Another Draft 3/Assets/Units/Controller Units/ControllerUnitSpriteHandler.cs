eb8c627e2f4c01b77cb0a0bb6780b2d0c428ac94
tjjoris <tjjoris@gmail.com>
2022-10-25 13:52:46 -0400

    I havea. duplicate of UnitStatsMultiplier

---
+++ Rechrysalis Another Draft 3/Assets/Units/Controller Units/ControllerUnitSpriteHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rechrysalis.Unit
+{
+    public class ControllerUnitSpriteHandler : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+
+        public void SetSpriteFunction (Sprite _sprite) {
+            _spriteRenderer.sprite = _sprite;
+            
+        }
+    }
+}
