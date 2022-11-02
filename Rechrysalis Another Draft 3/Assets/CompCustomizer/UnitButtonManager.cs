ce23d388dc1dfaca89d42a7615a60de0808d0ab6
tjjoris <tjjoris@gmail.com>
2022-11-02 16:08:14 -0400

    unit button

---
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/UnitButtonManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rechrysalis
+{
+    public class UnitButtonManager : MonoBehaviour
+    {
+        public void Initialize()
+        {
+
+        }
+        public void ClickUnitButton()
+        {
+            
+        }
+    }
+}
