3ec88777d86321c44c38bbf1bfbb62ab0571527b
tjjoris <tjjoris@gmail.com>
2022-11-07 09:21:49 -0500

    Adjusted hatch effect offset and font size

--- Rechrysalis Another Draft 3/Assets/Units/Hatch Effects/HEDisplay.cs
+++ Rechrysalis Another Draft 3/Assets/Units/Hatch Effects/HEDisplay.cs
@@ -6,7 +6,7 @@ namespace Rechrysalis.HatchEffect
 {
     public class HEDisplay : MonoBehaviour
     {
-        private Vector2 _offset = new Vector2(0.4f, -0.4f);
+        private Vector2 _offset = new Vector2(0.2f, -0.2f);
         public void PositionOffset(int _multiplier)
         {
             transform.localPosition = (_offset * _multiplier);
