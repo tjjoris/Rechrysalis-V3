5d31904ee036a9327da47659952c0accb7512d95
tjjoris <tjjoris@gmail.com>
2022-12-05 22:29:13 -0500

    creating vertical GO's and saving them to an array

--- Rechrysalis Another Draft 3/Assets/CompCustomizer/CompInitialize.cs
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/CompInitialize.cs
@@ -7,6 +7,8 @@ namespace Rechrysalis.CompCustomizer
     public class CompInitialize : MonoBehaviour
     {
         private CompCustomizerSO _compCustomizerSO;
+        [SerializeField] private GameObject _compButtonPrefab;
+        [SerializeField] private GameObject[] _verticalGOs;
 
         public void Initialize(CompCustomizerSO compCustomizerSO)
         {
