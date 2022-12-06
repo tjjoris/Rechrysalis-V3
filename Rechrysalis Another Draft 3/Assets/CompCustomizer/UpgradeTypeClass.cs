a064aaf3a0a3cb1a85c6e3a2001f1178c1c9c1c5
tjjoris <tjjoris@gmail.com>
2022-12-06 14:23:48 -0500

    storing unitStatsSO and hatchEffectSO in unitTypeClass

--- Rechrysalis Another Draft 3/Assets/CompCustomizer/UpgradeTypeClass.cs
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/UpgradeTypeClass.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rechrysalis.Unit;
+using Rechrysalis.HatchEffect;
 
 namespace Rechrysalis
 {
@@ -10,6 +12,8 @@ namespace Rechrysalis
             public enum UpgradeType { Basic, Advanced, HatchEffect, Error };
             [SerializeField] private UpgradeType _upgradeType;
             public UpgradeType UnitTypeVar { get { return _upgradeType; } set { _upgradeType = value; } }
+            [SerializeField] private UnitStatsSO _unitStatsSO;
+            [SerializeField] private HatchEffectSO _hatchEffectSO;
 
             public void SetUpgradeType(UpgradeType upgradeType)
             {
@@ -18,6 +22,22 @@ namespace Rechrysalis
             public UpgradeType GetUpgradeType()
             {
                 return _upgradeType;
+            }
+            public void SetUnitStatsSO(UnitStatsSO value)
+            {
+                _unitStatsSO = value;
+            }
+            public UnitStatsSO GetUnitStatsSO()
+            {
+                return _unitStatsSO;
+            }
+            public void SetHatchEffectSO(HatchEffectSO value)
+            {
+                _hatchEffectSO = value;                
+            }
+            public HatchEffectSO GetHatchEffectSO()
+            {
+                return _hatchEffectSO;
             }
         }
 }
