b0d603db7a88d608a0d071ff23d14bf84294000b
tjjoris <tjjoris@gmail.com>
2022-12-06 15:14:34 -0500

    still working on duplicate check

--- Rechrysalis Another Draft 3/Assets/CompCustomizer/RandomUpgradeSelection.cs
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/RandomUpgradeSelection.cs
@@ -26,24 +26,45 @@ namespace Rechrysalis.CompCustomizer
         {
             _randomIndex = Random.Range(0, selectionCount);
         }
-        private void CheckIfDuplicate(int[] _upgradeSelectionIndex, int selectionCount)
+        private void CheckIfDuplicate(int[] upgradeSelectionIndex, int selectionCount)
         {
             _selectionIndexToSelection.UpgradeFromIndex(_randomIndex);
             HatchEffectSO hatchEffectSOToCompare = null;
             UnitStatsSO unitStatsSOToCompare = null;
             UpgradeTypeClass.UpgradeType upgradeTypeToCompare = UpgradeTypeClass.UpgradeType.Error;
             // bool isDuplicate = false;
-            for (int i = 0; i < _upgradeSelectionIndex.Length; i++)
+            for (int i = 0; i < upgradeSelectionIndex.Length; i++)
             {
                 // if (_upgradeSelectionIndex[i] == _randomIndex)
-                if ((_selectionIndexToSelection.GetThisUpgradeType() == UpgradeTypeClass.UpgradeType.HatchEffect) && (_selectionIndexToSelection.GetHatchEffectSO == _selectionIndexToSelection.GetUpgradeTypeWithoutChanging(_randomIndex )))
+                // if (_selectionIndexToSelection.GetThisUpgradeType() == UpgradeTypeClass.UpgradeType.HatchEffect) 
+                // {
+                _selectionIndexToSelection.GetUpgradeTypeWithoutChanging(ref upgradeSelectionIndex[i], ref unitStatsSOToCompare, ref hatchEffectSOToCompare, ref upgradeTypeToCompare);
+                if (_selectionIndexToSelection.GetThisUpgradeType() == upgradeTypeToCompare)
                 {
-                    // isDuplicate = true;
-                    GetRandomNumber(selectionCount);
-                    CheckIfDuplicate(_upgradeSelectionIndex, selectionCount);
-                    return;
+                    if ((upgradeTypeToCompare == UpgradeTypeClass.UpgradeType.Basic) || (upgradeTypeToCompare == UpgradeTypeClass.UpgradeType.Advanced))
+                    {
+                        if (unitStatsSOToCompare == _selectionIndexToSelection.GetUnitStatsSO())
+                        {
+                            DuplicateFoundGetNew(selectionCount, upgradeSelectionIndex);
+                        }
+                    }
                 }
+                    // (_selectionIndexToSelection.GetHatchEffectSO() == _selectionIndexToSelection.GetUpgradeTypeWithoutChanging(_randomIndex, unitStatsSOToCompare, hatchEffectSOToCompare, upgradeTypeToCompare));
+            // }
+                // {
+                //     // isDuplicate = true;
+                //     GetRandomNumber(selectionCount);
+                //     CheckIfDuplicate(_upgradeSelectionIndex, selectionCount);
+                //     return;
+                // }
             }
+        }
+        private void DuplicateFoundGetNew(int selectionCount, int[] upgradeSelectionIndex)
+        {
+            Debug.Log($"duplicate found");
+            GetRandomNumber(selectionCount);
+            CheckIfDuplicate(upgradeSelectionIndex, selectionCount);
+            return;
         }
         public int GetRandomIndex()
         {
