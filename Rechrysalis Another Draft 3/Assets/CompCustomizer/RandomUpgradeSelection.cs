65dedfb6708ec3c454699a9be32a74dacde332db
tjjoris <tjjoris@gmail.com>
2022-12-06 15:17:47 -0500

    working on duplicate check for upgrades

--- Rechrysalis Another Draft 3/Assets/CompCustomizer/RandomUpgradeSelection.cs
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/RandomUpgradeSelection.cs
@@ -15,10 +15,10 @@ namespace Rechrysalis.CompCustomizer
         {
             _selectionIndexToSelection = GetComponent<SelectionIndexToSelection>();
         }
-        public int GetRandomSelection(CompCustomizerSO _compCustomizerSO, int[] _upgradeSelectionIndex, int selectionCount)
+        public int GetRandomSelection(CompCustomizerSO compCustomizerSO, int[] upgradeSelectionIndexArray, int selectionCount)
         {
             GetRandomNumber(selectionCount);
-            CheckIfDuplicate(_upgradeSelectionIndex, selectionCount);
+            CheckIfDuplicate(upgradeSelectionIndexArray, selectionCount);
             return _randomIndex;
             
         }
@@ -26,26 +26,33 @@ namespace Rechrysalis.CompCustomizer
         {
             _randomIndex = Random.Range(0, selectionCount);
         }
-        private void CheckIfDuplicate(int[] upgradeSelectionIndex, int selectionCount)
+        private void CheckIfDuplicate(int[] upgradeSelectionIndexArray, int selectionCount)
         {
             _selectionIndexToSelection.UpgradeFromIndex(_randomIndex);
             HatchEffectSO hatchEffectSOToCompare = null;
             UnitStatsSO unitStatsSOToCompare = null;
             UpgradeTypeClass.UpgradeType upgradeTypeToCompare = UpgradeTypeClass.UpgradeType.Error;
             // bool isDuplicate = false;
-            for (int i = 0; i < upgradeSelectionIndex.Length; i++)
+            for (int i = 0; i < upgradeSelectionIndexArray.Length; i++)
             {
                 // if (_upgradeSelectionIndex[i] == _randomIndex)
                 // if (_selectionIndexToSelection.GetThisUpgradeType() == UpgradeTypeClass.UpgradeType.HatchEffect) 
                 // {
-                _selectionIndexToSelection.GetUpgradeTypeWithoutChanging(ref upgradeSelectionIndex[i], ref unitStatsSOToCompare, ref hatchEffectSOToCompare, ref upgradeTypeToCompare);
+                _selectionIndexToSelection.GetUpgradeTypeWithoutChanging(ref upgradeSelectionIndexArray[i], ref unitStatsSOToCompare, ref hatchEffectSOToCompare, ref upgradeTypeToCompare);
                 if (_selectionIndexToSelection.GetThisUpgradeType() == upgradeTypeToCompare)
                 {
                     if ((upgradeTypeToCompare == UpgradeTypeClass.UpgradeType.Basic) || (upgradeTypeToCompare == UpgradeTypeClass.UpgradeType.Advanced))
                     {
                         if (unitStatsSOToCompare == _selectionIndexToSelection.GetUnitStatsSO())
                         {
-                            DuplicateFoundGetNew(selectionCount, upgradeSelectionIndex);
+                            DuplicateFoundGetNew(selectionCount, upgradeSelectionIndexArray);
+                        }
+                    }
+                    if (upgradeTypeToCompare == UpgradeTypeClass.UpgradeType.HatchEffect)
+                    {
+                        if (hatchEffectSOToCompare == _selectionIndexToSelection.GetHatchEffectSO())
+                        {
+                            DuplicateFoundGetNew(selectionCount, upgradeSelectionIndexArray);
                         }
                     }
                 }
