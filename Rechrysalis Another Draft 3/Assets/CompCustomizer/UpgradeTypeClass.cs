c4bc47ad3c82b644566e05eefe8cb72c1204871a
tjjoris <tjjoris@gmail.com>
2022-12-05 21:29:21 -0500

    more upgrade type class work

---
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/UpgradeTypeClass.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rechrysalis
+{
+    public class UpgradeTypeClass
+        {
+            public enum UpgradeType { Basic, Advanced, HatchEffect, Error };
+            [SerializeField] private UpgradeType _upgradeType;
+            public UpgradeType UnitTypeVar { get { return _upgradeType; } set { _upgradeType = value; } }
+
+            public void SetUpgradeType(UpgradeType upgradeType)
+            {
+                _upgradeType = upgradeType;
+            }
+            public UpgradeType GetUpgradeType()
+            {
+                return _upgradeType;
+            }
+        }
+}
