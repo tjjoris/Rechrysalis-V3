0728f83dca74e18427c2279c0df473b8d375b7c4
tjjoris <tjjoris@gmail.com>
2022-12-06 17:07:18 -0500

    comp initialze subscribing to vertical managers buttons

--- Rechrysalis Another Draft 3/Assets/CompCustomizer/CompInitialize.cs
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/CompInitialize.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Rechrysalis.Unit;
+using System;
 
 namespace Rechrysalis.CompCustomizer
 {
@@ -10,11 +11,48 @@ namespace Rechrysalis.CompCustomizer
         private CompCustomizerSO _compCustomizerSO;
         [SerializeField] private GameObject _compButtonPrefab;
         [SerializeField] private CompVerticalManager[] _verticalMangers;
+        public Action<CompUpgradeManager> _onCompUpgradeClicked;
 
+        public void OnEnable()
+        {
+            SubscribeToVerticalManagers();
+        }
+        public void SubscribeToVerticalManagers()        
+        {
+            if (_verticalMangers != null)
+            {
+                for (int _index = 0; _index < _verticalMangers.Length; _index ++)
+                {
+                    if (_verticalMangers[_index] != null)
+                    {
+                        _verticalMangers[_index]._onCompUpgradeClicked -= CompUpgradeClicked;
+                        _verticalMangers[_index]._onCompUpgradeClicked += CompUpgradeClicked;
+                    }
+                }
+            }
+        }
+        private void OnDisable()
+        {
+            if (_verticalMangers != null)
+            {
+                for (int _index = 0; _index < _verticalMangers.Length; _index++)
+                {
+                    if (_verticalMangers[_index] != null)
+                    {
+                        _verticalMangers[_index]._onCompUpgradeClicked -= CompUpgradeClicked;
+                    }
+                }
+            }
+        }
         public void Initialize(CompCustomizerSO compCustomizerSO, CompSO playerComp)
         {
             _compCustomizerSO = compCustomizerSO;  
             LoopVerticals(playerComp);
+            SubscribeToVerticalManagers();
+        }
+        private void CompUpgradeClicked(CompUpgradeManager compUpgradeManager)
+        {
+            _onCompUpgradeClicked?.Invoke(compUpgradeManager);            
         }
         private void LoopVerticals(CompSO playerComp)
         {
