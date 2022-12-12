0372536c01397ffd3b70a051daf4eac7f471014d
tjjoris <tjjoris@gmail.com>
2022-12-12 17:03:56 -0500

    made parent unit class

---
+++ Rechrysalis Another Draft 3/Assets/Units/ParentUnitClass.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rechrysalis.Unit
+{
+    [System.Serializable]
+    public class ParentUnitClass
+    {
+        [SerializeField] private List<UpgradeTypeClass> _advancedUpgradesUTCList;
+        public List<UpgradeTypeClass> AdvancedUpgradesUTCList { get{ return _advancedUpgradesUTCList; } set{ _advancedUpgradesUTCList = value; } }
+        [SerializeField] private UpgradeTypeClass _utcBasicUnit;
+        public UpgradeTypeClass UTCBasicUnit { get{ return _utcBasicUnit; } set{ _utcBasicUnit = value; } }
+        [SerializeField] private UpgradeTypeClass _utcHatchEffect;
+        public UpgradeTypeClass UTCHatchEffect { get{ return _utcHatchEffect; } set{ _utcHatchEffect = value; } }
+        private UpgradeTypeClass _replacedUTCBasicUnit;
+        private UpgradeTypeClass _replaceUTCHatchEffect;
+        
+        public void SetUTCBasicUnit(UpgradeTypeClass utcBasicUnit)
+        {
+            if (utcBasicUnit != null)
+            {
+                if (_utcBasicUnit != null)
+                {
+                    _replacedUTCBasicUnit = _utcBasicUnit;
+                }   
+                _utcBasicUnit = utcBasicUnit;
+            }
+        }
+        public UpgradeTypeClass GetReplacedUTCBasicUnit()
+        {
+            return _replacedUTCBasicUnit;
+        }
+        public void SetUTCReplacedBacsicUnitToNull()
+        {
+            _replacedUTCBasicUnit = null;
+        }
+        public void SetUTCHatchEffect(UpgradeTypeClass utcHatchEffect)
+        {
+            if (utcHatchEffect != null)
+            {
+                if (_utcHatchEffect != null)
+                {
+                    _replaceUTCHatchEffect = _utcHatchEffect;
+                }
+                _utcHatchEffect = utcHatchEffect;
+            }
+        }
+        public UpgradeTypeClass GetReplacedUTCHatchEffect()
+        {
+            return _replaceUTCHatchEffect;
+        }
+        public void SetUTCReplacedHatchEffectToNull()
+        {
+            _replaceUTCHatchEffect = null;
+        }
+        public void AddUTCAdvanced(UpgradeTypeClass advancedToAdd)
+        {
+            if (advancedToAdd != null)
+            {
+                _advancedUpgradesUTCList.Add(advancedToAdd);
+            }
+        }
+        public void RemoveUTCAdvanced(UpgradeTypeClass advancedToRemove)
+        {
+            if (advancedToRemove != null)
+            {
+                if (_advancedUpgradesUTCList.Contains(advancedToRemove))
+                {                    
+                    _advancedUpgradesUTCList.Remove(advancedToRemove);
+                }
+            }
+        }
+    }
+}
