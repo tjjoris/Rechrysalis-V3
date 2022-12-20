6ebc4c313840f4634b6e2e38a14378eb14f7c148
tjjoris <tjjoris@gmail.com>
2022-12-20 11:07:54 -0500

    making parent unit class contain some stats

--- Rechrysalis Another Draft 3/Assets/Units/ParentUnitClass.cs
+++ Rechrysalis Another Draft 3/Assets/Units/ParentUnitClass.cs
@@ -15,6 +15,26 @@ namespace Rechrysalis.Unit
         public UpgradeTypeClass UTCHatchEffect { get{ return _utcHatchEffect; } set{ _utcHatchEffect = value; } }
         private UpgradeTypeClass _replacedUTCBasicUnit;
         private UpgradeTypeClass _replaceUTCHatchEffect;
+        [SerializeField] private float _manaCost;
+        public float ManaCost => _manaCost;
+        [SerializeField] private float _hpMax;
+        public float HPMax => _hpMax;
+        [SerializeField] private float _buildTime;
+        public float BuildTime => _buildTime;
+        [SerializeField] private float _range;
+        public float Range => _range;
+        [SerializeField] private float _dps;
+        public float DPS => _dps;
+        [SerializeField] private float _attackChargeUp;
+        public float AttackChargeUp => _attackChargeUp;
+        [SerializeField] private float _attackWindDown;
+        public float AttackWindDown => _attackWindDown; 
+        [SerializeField] private float _damage;
+        public float Damamge => _damage;
+        [SerializeField] private GameObject _hatchEffectPrefab;
+        public GameObject HatchEffectPrefab => _hatchEffectPrefab;
+        [SerializeField] private float _hatchEffectMult;
+        public float HatchEffectMult => _hatchEffectMult;
         
         public void ClearAllUpgrades()
         {
