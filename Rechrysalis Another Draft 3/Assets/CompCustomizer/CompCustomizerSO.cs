4ea5931534a78782af77de9716a0c67c82500b51
tjjoris <tjjoris@gmail.com>
2022-12-20 12:38:11 -0500

    making comp customizer SO contain adv upgrade modifiere SO instead of Unit stats SO array for adv units

--- Rechrysalis Another Draft 3/Assets/CompCustomizer/CompCustomizerSO.cs
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/CompCustomizerSO.cs
@@ -14,8 +14,8 @@ namespace Rechrysalis.CompCustomizer
         public int NumberOfUpgrades { get{ return _numberOfUpgrades; } set{ _numberOfUpgrades = value; } }
         [SerializeField] private UnitStatsSO[] _basicUnitSelectionArray;
         public UnitStatsSO[] BasicUnitArray { get{ return _basicUnitSelectionArray; }}
-        [SerializeField] private UnitStatsSO[] _advancedUnitSelectionT1Array;
-        public UnitStatsSO[] AdvancedUnitSelectionT1Array { get{ return _advancedUnitSelectionT1Array; }}
+        [SerializeField] private AdvUnitModifierSO[] _advancedUnitSelectionArray;
+        public AdvUnitModifierSO[] AdvancedUnitSelectionT1Array { get{ return _advancedUnitSelectionArray; }}
         [SerializeField] private HatchEffectSO[] _hatchEffectSelectionArray;
         public HatchEffectSO[] HatchEffectSelectionArray { get{ return _hatchEffectSelectionArray; }}
 
