56edbaace9dff64a0bd0c98a8f2c10c6711e4b91
tjjoris <tjjoris@gmail.com>
2022-12-22 17:36:44 -0500

    deleting unused

--- Rechrysalis Another Draft 3/Assets/CompCustomizer/CompUpgradeManager.cs
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/CompUpgradeManager.cs
@@ -11,10 +11,6 @@ namespace Rechrysalis.CompCustomizer
     public class CompUpgradeManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler//, IPointerDownHandler, IPointerMoveHandler
     {
         private bool debugBool = false;
-        // [SerializeField] private int _parentIndex;
-        // public int ParentIndex { get{ return _parentIndex; } set{ _parentIndex = value; } }
-        // [SerializeField] private int _childIndex;
-        // public int ChildIndex { get{ return _childIndex; } set{ _childIndex = value; } }
         private Transform _movingButtonHolder;
         [SerializeField] private Transform _parentAfterDrag;
         public Transform ParentAfterDrag { get{ return _parentAfterDrag; } set{ _parentAfterDrag = value; } }
@@ -25,25 +21,6 @@ namespace Rechrysalis.CompCustomizer
         
         [SerializeField] private UpgradeTypeClass _upgradeTypeClass;
         private UpgradeButtonDisplay _upgradeButtonDisplay;
-        // private float _holdTimerCurrent;
-        // private float _holdTimerMax = 0.4f;
-        // private bool _upgradeClickedPointerWithin;
-        // private bool _buttonHeldToMove;
-        // private bool _pointerWithinButton;
-        // private float _yPointerStart;
-        // private float _yMaxYDistAllowedToHold = 15;
-        // public Action<CompUpgradeManager> _onCompUpgradeClicked;    
-        // public Action _disableVerticalScroll;
-        // public void InitializeOldStillUsed(int parentIndex, int childIndex, Transform movingButtonHolder)
-        // {
-        //     _movingButtonHolder = movingButtonHolder;
-        //     _parentIndex = parentIndex;
-        //     _childIndex = childIndex;
-        //     // _upgradeType = new UpgradeTypeClass();
-        //     _upgradeButtonDisplay = GetComponent<UpgradeButtonDisplay>();
-        //     // _upgradeButtonDisplay.Initialzie();
-        //     _image = GetComponent<Image>();
-        // }
         public void Initialize(Transform movingButtonHolder)
         {
             _movingButtonHolder = movingButtonHolder;
@@ -70,44 +47,7 @@ namespace Rechrysalis.CompCustomizer
         public void SetDisplay(UpgradeTypeClass upgradeTypeClass)
         {
             _upgradeButtonDisplay.SetButotnDisplay(upgradeTypeClass);
-        }
-        // public void OnPointerMove(PointerEventData pointerData)
-        // {
-        //     if (_buttonHeldToMove)
-        //     transform.position = Input.mousePosition;
-        // }
-        // public void OnPointerDown(PointerEventData pointerData)
-        // {
-        //     CompUpgradeClicked();
-        // }
-        // public void CompUpgradeClicked()
-        // {
-        //     // _upgradeClickedPointerWithin = true;
-        //     // _pointerWithinButton = true;
-        //     // _buttonHeldToMove = false;            
-        //     // _holdTimerCurrent = 0;
-        //     // _yPointerStart = Input.mousePosition.y;
-        //     // _onCompUpgradeClicked?.Invoke(this);
-        // }
-        // private void Update()
-        // {
-        //     // if (_upgradeClickedPointerWithin && !_buttonHeldToMove) {
-        //     //     _holdTimerCurrent += Time.deltaTime;  
-        //     //     // _upgradeClickedPointerWithin = EventSystem.current.IsPointerOverGameObject();                       
-        //     //     if (Mathf.Abs(_yPointerStart - Input.mousePosition.y) > _yMaxYDistAllowedToHold)
-        //     //     {
-        //     //         _upgradeClickedPointerWithin = false;
-        //     //     }
-        //     //     if ((_upgradeClickedPointerWithin)&& (_holdTimerCurrent >= _holdTimerMax))
-        //     //     {
-        //     //         _buttonHeldToMove = true;
-        //     //         transform.SetParent(_movingButtonHolder.transform);
-        //     //         _disableVerticalScroll?.Invoke();
-        //     //     }
-        //     // }            
-        // }
-
-        public void OnBeginDrag(PointerEventData eventData)
+        }        public void OnBeginDrag(PointerEventData eventData)
         {
             _siblingIndex = transform.GetSiblingIndex();
             _parentAfterDrag = transform.parent;
