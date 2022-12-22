83bc2899a5b592e4bb2b1a2f50b9ceac51a662b5
tjjoris <tjjoris@gmail.com>
2022-12-22 17:44:39 -0500

    more deleted

--- Rechrysalis Another Draft 3/Assets/CompCustomizer/SelectionIndexToSelection.cs
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/SelectionIndexToSelection.cs
@@ -14,42 +14,17 @@ namespace Rechrysalis.CompCustomizer
         private int _upgradeBasicSelectionCount;
         private int _upgradeT1SelectionCount;
         private int _upgradeHatchEffectCount;
-        // private UnitStatsSO _unitStatsSO;
-        // private HatchEffectSO _hatchEffectSO;
         private RandomUpgradeSelection _randomUpgradeSelection;
-        // public enum UpgradeType {Basic, Advanced, HatchEffect, Error};
-        // [SerializeField] private UpgradeTypeClass _upgradeTypeClass;
         
         
         public void Initialize(CompCustomizerSO compCustomizerSO)
         {
-            // _upgradeTypeClass = new UpgradeTypeClass();
             _randomUpgradeSelection = GetComponent<RandomUpgradeSelection>();
             _compCustomizerSO = compCustomizerSO;        
             _upgradeBasicSelectionCount = _compCustomizerSO.BasicUnitArray.Length;
             _upgradeT1SelectionCount = _compCustomizerSO.AdvancedUnitSelectionT1Array.Length;
             _upgradeHatchEffectCount = _compCustomizerSO.HatchEffectSelectionArray.Length;
         }
-        // public void UpgradeFromIndex(int upgradeIndex)
-        // {
-        //     if (upgradeIndex < _upgradeBasicSelectionCount)
-        //     {
-        //         _upgradeTypeClass.SetUnitStatsSO(_compCustomizerSO.BasicUnitArray[upgradeIndex]);
-        //         _upgradeTypeClass.SetHatchEffectSO(null);
-        //         SetUpgradeTypeClass(UpgradeTypeClass.UpgradeType.Basic);              
-        //     }
-        //     else if (upgradeIndex < (_upgradeBasicSelectionCount + _upgradeT1SelectionCount))
-        //     {
-        //         _upgradeTypeClass.SetUnitStatsSO(_compCustomizerSO.AdvancedUnitSelectionT1Array[upgradeIndex - _upgradeBasicSelectionCount]);
-        //         _upgradeTypeClass.SetHatchEffectSO(null);
-        //         SetUpgradeTypeClass(UpgradeTypeClass.UpgradeType.Advanced);
-        //     }
-        //     else {
-        //         _upgradeTypeClass.SetUnitStatsSO(null);
-        //         _upgradeTypeClass.SetHatchEffectSO(_compCustomizerSO.HatchEffectSelectionArray[upgradeIndex - _upgradeBasicSelectionCount - _upgradeT1SelectionCount]);
-        //         SetUpgradeTypeClass(UpgradeTypeClass.UpgradeType.HatchEffect);
-        //     }
-        // }
         public UpgradeTypeClass GetUpgradeTypeClassFromIndex(int index)
         {
             if (debugBool)
@@ -66,45 +41,5 @@ namespace Rechrysalis.CompCustomizer
                 return _compCustomizerSO.HatchEffectSelectionArray[index - _upgradeBasicSelectionCount - _upgradeT1SelectionCount].UpgradeTypeClass;
             }
         }
-        // public void GetUpgradeTypeWithoutChanging(ref int upgradeIndex, ref UnitStatsSO unitStatsSO, ref HatchEffectSO hatchEffectSO, ref UpgradeTypeClass.UpgradeType upgradeType)
-        // {
-        //     // if (upgradeIndex < _upgradeBasicSelectionCount)
-        //     // {
-        //     //     unitStatsSO = _compCustomizerSO.BasicUnitArray[upgradeIndex];
-        //     //     hatchEffectSO = null;
-        //     //     upgradeType = UpgradeTypeClass.UpgradeType.Basic;
-        //     // }
-        //     // else if (upgradeIndex < (_upgradeBasicSelectionCount + _upgradeT1SelectionCount))
-        //     // {
-        //     //     unitStatsSO = _compCustomizerSO.AdvancedUnitSelectionT1Array[upgradeIndex - _upgradeBasicSelectionCount];
-        //     //     hatchEffectSO = null;
-        //     //     upgradeType = UpgradeTypeClass.UpgradeType.Advanced;
-        //     // }
-        //     // else {
-        //     //     unitStatsSO = null;
-        //     //     hatchEffectSO = _compCustomizerSO.HatchEffectSelectionArray[upgradeIndex - _upgradeBasicSelectionCount - _upgradeT1SelectionCount];
-        //     //     upgradeType = UpgradeTypeClass.UpgradeType.HatchEffect;
-        //     // }
-        // }
-        // private void SetUpgradeTypeClass(UpgradeTypeClass.UpgradeType upgradeType)
-        // {
-        //     _upgradeTypeClass.SetUpgradeType(upgradeType);
-        // }
-        // public UnitStatsSO GetUnitStatsSO()
-        // {
-        //     return _upgradeTypeClass.GetUnitStatsSO();
-        // }
-        // public HatchEffectSO GetHatchEffectSO()
-        // {
-        //     return _upgradeTypeClass.GetHatchEffectSO();
-        // }
-        // public UpgradeTypeClass.UpgradeType GetThisUpgradeType()
-        // {
-        //     return _upgradeTypeClass.GetUpgradeType();
-        // }
-        // public UpgradeTypeClass GetUpgradeTypeClass()
-        // {
-        //     return _upgradeTypeClass;
-        // }
     }
 }
