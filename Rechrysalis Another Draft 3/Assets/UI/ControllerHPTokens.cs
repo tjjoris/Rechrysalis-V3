0063c292144b064f2ea224ede45e234dcf08a9b5
tjjoris <tjjoris@gmail.com>
2023-01-02 17:34:48 -0500

    logic for controller hp token script

--- Rechrysalis Another Draft 3/Assets/UI/ControllerHPTokens.cs
+++ Rechrysalis Another Draft 3/Assets/UI/ControllerHPTokens.cs
@@ -7,6 +7,20 @@ namespace Rechrysalis.Controller
 {
     public class ControllerHPTokens : MonoBehaviour
     {
-        
+        [SerializeField] private GameObject[] _controllerHPTokens;
+        [SerializeField] private int _tokenActiveIndex = 3;
+        [SerializeField] private int _tokenMax = 3;
+
+        public void RemoveToken()
+        {
+            _controllerHPTokens[_tokenActiveIndex].SetActive(false);
+            _tokenActiveIndex --;
+        }
+        public void AddTokens(int count)
+        {
+            for (int i = 0; i<count; i++)
+            _controllerHPTokens[_tokenActiveIndex].SetActive(true);
+            _tokenActiveIndex ++;
+        }
     }
 }
