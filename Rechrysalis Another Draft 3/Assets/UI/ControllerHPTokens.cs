04b196572847769ea328058ed8bf3237c1a1105f
tjjoris <tjjoris@gmail.com>
2023-01-02 17:30:48 -0500

    script for controller hp tokens

---
+++ Rechrysalis Another Draft 3/Assets/UI/ControllerHPTokens.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Rechrysalis.Controller
+{
+    public class ControllerHPTokens : MonoBehaviour
+    {
+        
+    }
+}
