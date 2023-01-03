3da2c599bdb723810525d89fae3431b9058e3c85
tjjoris <tjjoris@gmail.com>
2023-01-03 11:16:49 -0500

    is it working?

---
+++ Rechrysalis Another Draft 3/Assets/CompCustomizer/DropSelection.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rechrysalis.CompCustomizer
+{
+    public class DropSelection : DropBackGround
+    {
+        protected override void DropUpgrade(CompUpgradeManager compUpgradeManager)
+        {
+            base.DropUpgrade(compUpgradeManager);
+        }
+    }
+}
