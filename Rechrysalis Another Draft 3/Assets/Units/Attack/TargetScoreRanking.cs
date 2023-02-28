680e48b9140be142cab42fc59f73e2e936138839
tjjoris <tjjoris@gmail.com>
2023-02-28 11:51:11 -0500

    trying to eliminate the infinate loop

--- Rechrysalis Another Draft 3/Assets/Units/Attack/TargetScoreRanking.cs
+++ Rechrysalis Another Draft 3/Assets/Units/Attack/TargetScoreRanking.cs
@@ -31,15 +31,15 @@ namespace Rechrysalis.Attacking
         private void AddToScoreRank(ParentUnitManager parentUnitManager)
         {
             float currentScoreValue = parentUnitManager.TargetScoreValue.CurrentScoreValue;
-            for (int i = 0; i < _scoresRanked.Count; i++)
-            {
-                int j=i--;
-                if (((j<0) || (_scoresRanked[j].TargetScoreValue.CurrentScoreValue > currentScoreValue)) && ((_scoresRanked[i].TargetScoreValue.CurrentScoreValue < currentScoreValue)))
-                {
-                    _scoresRanked.Insert( i, parentUnitManager);
-                    return;
-                }
-            }
+            // for (int i = 0; i < _scoresRanked.Count; i++)
+            // {
+            //     int j=i--;
+            //     if (((j<0) || (_scoresRanked[j].TargetScoreValue.CurrentScoreValue > currentScoreValue)) && ((_scoresRanked[i].TargetScoreValue.CurrentScoreValue < currentScoreValue)))
+            //     {
+            //         _scoresRanked.Insert( i, parentUnitManager);
+            //         return;
+            //     }
+            // }
             _scoresRanked.Add(parentUnitManager);
         }
     }
