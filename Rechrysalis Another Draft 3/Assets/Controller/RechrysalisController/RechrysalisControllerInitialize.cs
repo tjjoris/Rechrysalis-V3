e41ec1231e6793d28f7aa34735978a5022bde3bb
tjjoris <tjjoris@gmail.com>
2023-03-07 09:39:34 -0500

    working

--- Rechrysalis Another Draft 3/Assets/Controller/RechrysalisController/RechrysalisControllerInitialize.cs
+++ Rechrysalis Another Draft 3/Assets/Controller/RechrysalisController/RechrysalisControllerInitialize.cs
@@ -158,7 +158,7 @@ namespace Rechrysalis.Controller
             chrysalisGo.GetComponent<UnitManager>()?.Initialize(_controllerIndex, unitClass, parentUnitIndex, compsAndUnits);
             chrysalisGo.GetComponent<UnitManager>()?.SetUnitSPrite(unitClass.ChrysalisSprite);            
             // _chrysalisManager.SetUnitName(_unitStats.UnitName);
-            chrysalisGo.GetComponent<ChrysalisTimer>()?.Initialize(unitClass.BuildTime, childUnitIndex, _controllerManager.GetComponent<ProgressBarManager>());
+            chrysalisGo.GetComponent<ChrysalisTimer>()?.Initialize(unitClass.BuildTime, childUnitIndex, pum.GetComponent<ProgressBarManager>());
             pum.SubChrysalii[childUnitIndex] = chrysalisGo;
             _allUnits.Add(chrysalisGo);
             _controllerHatchEffect.SetUnitsArray(chrysalisGo, ((parentUnitIndex * 6) + (childUnitIndex * 2) + 1));
