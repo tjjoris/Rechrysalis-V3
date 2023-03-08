e506430b524bc33ba4e9bc8580ef8c6bef981823
tjjoris <tjjoris@gmail.com>
2023-03-08 15:58:22 -0500

    making activate chrysalis and unit in hilight ring parent unit

--- Rechrysalis Another Draft 3/Assets/Controller/HilightRing/HilightRingUnitManager.cs
+++ Rechrysalis Another Draft 3/Assets/Controller/HilightRing/HilightRingUnitManager.cs
@@ -7,5 +7,18 @@ namespace Rechrysalis.Controller
     public class HilightRingUnitManager : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer _body;
+
+        public void SetSprite(Sprite sprite)
+        {
+            _body.sprite = sprite;
+        }
+        public void EnableGO()
+        {
+            gameObject.SetActive(true);
+        }
+        public void DisableGO()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
