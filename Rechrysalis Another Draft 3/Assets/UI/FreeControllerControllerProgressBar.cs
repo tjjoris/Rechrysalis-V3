891834e203906872a1a3d44a705f3dbd7bce2b4a
tjjoris <tjjoris@gmail.com>
2023-03-27 12:52:18 -0400

    creating free controller controller progress bar

---
+++ Rechrysalis Another Draft 3/Assets/UI/FreeControllerControllerProgressBar.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rechrysalis.UI
+{
+    public class FreeControllerControllerProgressBar : MonoBehaviour
+    {
+    }
+}
