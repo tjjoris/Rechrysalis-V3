2ae69dd63289be1c9b77b7386d848c5a6c897517
tjjoris <tjjoris@gmail.com>
2023-03-27 12:59:43 -0400

    Update FreeControllerControllerProgressBar.cs

--- Rechrysalis Another Draft 3/Assets/UI/FreeControllerControllerProgressBar.cs
+++ Rechrysalis Another Draft 3/Assets/UI/FreeControllerControllerProgressBar.cs
@@ -6,5 +6,27 @@ namespace Rechrysalis.UI
 {
     public class FreeControllerControllerProgressBar : MonoBehaviour
     {
+        [SerializeField] private RectTransform _progressFill;
+        [SerializeField] private float _progressMax;
+        public float ProgressMax { get => _progressMax; set => _progressMax = value; }
+        [SerializeField] private float _progressCurrent;
+        public float ProgressCurrent => _progressCurrent;
+
+        public void Initialize(float progressMax)
+        {
+            _progressMax = progressMax;
+            _progressCurrent = 0;
+            SetBarScale(0);
+        }
+        public void AddProgress(float amount)
+        {
+            _progressCurrent += amount;
+            float progressPercent = amount / _progressMax;
+            SetBarScale(progressPercent);
+        }
+        private void SetBarScale(float percent)
+        {
+            _progressFill.localScale = new Vector3(percent, 1, 1);
+        }
     }
 }
