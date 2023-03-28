53e1c9e51e6c337ceb31119ee7b0c584968e130f
tjjoris <tjjoris@gmail.com>
2023-03-28 16:31:45 -0400

    instantiate wave

--- Rechrysalis Another Draft 3/Assets/Levels/Waves/FreeEnemyWaveInstantiator.cs
+++ Rechrysalis Another Draft 3/Assets/Levels/Waves/FreeEnemyWaveInstantiator.cs
@@ -10,15 +10,28 @@ namespace Rechrysalis.Unit
         [SerializeField] private int _waveIndex = 0;
         private FreeEnemyWaveGenerator _freeEnemyWaveGenerator;
         [SerializeField] private CompsAndUnitsSO _compsAndUnitsSO;
+        private WaveLayoutsByRange _waveLayoutByRange;
+        private FreeEnemyInitialize _freeEnemyInitialize;
 
         public void Initialize(CompsAndUnitsSO compsAndUnitsSO)
         {
             _compsAndUnitsSO = compsAndUnitsSO;
             _freeEnemyWaveGenerator = GetComponent<FreeEnemyWaveGenerator>();
+            _freeEnemyInitialize = GetComponent<FreeEnemyInitialize>();
+            _waveLayoutByRange = _freeEnemyInitialize.WaveLayoutsByRange;
         }
         public void CreateWave()
         {
+            if (_freeEnemyWaveGenerator.WaveClassList.Count > _waveIndex)
+            {
+                if (_freeEnemyWaveGenerator.WaveClassList[_waveIndex].UnitsInWave.Count > 0)
+                {
+                    foreach (ParentUnitClass parentUnitClass in _freeEnemyWaveGenerator.WaveClassList[_waveIndex].UnitsInWave)
+                    {
 
+                    }
+                }
+            }
         }
     }
 }
