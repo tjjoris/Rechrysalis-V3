939c139bbb04306c5b8f739fdab52897984faa43
tjjoris <tjjoris@gmail.com>
2023-03-30 07:58:24 -0400

    build time faster with higher hp script

---
+++ Rechrysalis Another Draft 3/Assets/Units/Parents/BuildTimeFasterWithHigherHP.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rechrysalis.Unit
+{    
+    public class BuildTimeFasterWithHigherHP : MonoBehaviour
+    {
+        private ParentHealth _parentHealth;
+        private float[] _ratioThreshHolds = new float[3] {0.3333f, 0.6666f, 1f};
+        private float[] _buildSpeedMult = new float[3] {1, 0.5f, 0f};
+        private int _buildSPeedMultIndexToUse;
+
+        private void Awake()
+        {
+            Initaialize();
+        }
+        public void Initaialize()
+        {
+            _parentHealth = GetComponent<ParentHealth>();
+        }
+        public void FindBuildSpeedMult()
+        {
+            float healthRatio = _parentHealth.GetHealthRatio();
+            FindBuildSPeedMultIndex(healthRatio);            
+        }
+        private void FindBuildSPeedMultIndex(float healthRatio)
+        {
+            for (int i = 0; i < _ratioThreshHolds.Length; i++)
+            {
+                if (healthRatio <= _ratioThreshHolds[i])
+                {
+                    _buildSPeedMultIndexToUse = i;
+                    Debug.Log($"saved build speed mult " + _buildSpeedMult[i]);
+                    return;
+                }
+            }
+        }
+        public float GetBuildSpeedMult()
+        {
+            return _buildSpeedMult[_buildSPeedMultIndexToUse];
+        }
+    }
+}
