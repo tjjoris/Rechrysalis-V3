085ce049de89a21f48fb60963844c19c93eb1f53
tjjoris <tjjoris@gmail.com>
2023-04-09 19:55:00 -0400

    update PlayerPrefsInteract for infinity mode options

--- Rechrysalis Another Draft 3/Assets/UI/PlayerPrefsInteract.cs
+++ Rechrysalis Another Draft 3/Assets/UI/PlayerPrefsInteract.cs
@@ -8,6 +8,9 @@ namespace Rechrysalis.UI
     public class PlayerPrefsInteract : MonoBehaviour
     {
         private const string TARGETDURINGTARGETMODE = "TargetDuringTargetMode";
+        private const string HEALTHTOBUILDTIME  = "HealthToBuildTime";
+        private const string HASBASICUNIT = "HasBasicUnit";
+        private const string HASMANA = "HasMana";
         public Action _changePlayerPrefs;
 
         public void SetTargetDuringTargetMode(bool value)
@@ -23,6 +26,49 @@ namespace Rechrysalis.UI
         public bool GetTargetOnlyDuringTargetMode()
         {
             if (PlayerPrefs.GetInt(TARGETDURINGTARGETMODE) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void SetHealthToBuildTime(int value)
+        {
+            if ((value < 0) || (value > 2)) return;
+            PlayerPrefs.SetInt(HEALTHTOBUILDTIME, value);
+        }
+        public int GetHealthToBuildTime()
+        {
+            return PlayerPrefs.GetInt(HEALTHTOBUILDTIME);
+        }
+        public void SetHasBasicUnit(bool value)
+        {
+            if (value)
+            {
+                PlayerPrefs.SetInt(HASBASICUNIT, 1);
+                return;
+            }
+            PlayerPrefs.SetInt(HASBASICUNIT, 0);
+        }
+        public int GetHasBasicUnit()
+        {
+            if (PlayerPrefs.GetInt(HASBASICUNIT) == 1)
+            {
+            return true;
+            }
+            return false;
+        }
+        public void SetHasMana(bool value)
+        {
+            if (value)
+            {
+                PlayerPrefs.SetInt(HASMANA, 1);
+                return;
+            }
+            PlayerPrefs.SetInt(HASMANA, 0);
+        }
+        public bool GetHasMana()
+        {
+            if (PlayerPrefs.GetInt(HASMANA) == 1)
             {
                 return true;
             }
