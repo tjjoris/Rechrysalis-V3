31bcfc9357f88847a6fb386447ff48a294fa066b
tjjoris <tjjoris@gmail.com>
2023-04-13 11:03:48 -0400

    build time base add minus active on Hatch working

--- Rechrysalis Another Draft 3/Assets/Units/OnHatchEffects/HatchAdjustBuildTimerMaxBase.cs
+++ Rechrysalis Another Draft 3/Assets/Units/OnHatchEffects/HatchAdjustBuildTimerMaxBase.cs
@@ -7,26 +7,42 @@ namespace Rechrysalis.Unit
 {
     public class HatchAdjustBuildTimerMaxBase : MonoBehaviour
     {
+        private bool _debugBool = true;
+        private bool _buildTimeHasBeenAdjusted;
         private ControllerManager _controllerManager;
         private float _buildTimeToAdd;
         [SerializeField] private float _currentTime;
-        [SerializeField] private float _maxTime = 2;
+        [SerializeField] private float _maxTime = 4;
         public void Initialize(ControllerManager controllerManager, float buildTimeToAdd)
         {
             _buildTimeToAdd = buildTimeToAdd;
             _controllerManager = controllerManager;
+        }
+        public void Activate()
+        {
+            if (_debugBool) Debug.Log($"add negative build time");
+            _currentTime = 0;
+            _buildTimeHasBeenAdjusted = true;
             AddBuildTimeMax(_buildTimeToAdd);
         }
         public void Tick(float timeAmount)
         {
+            if (!_buildTimeHasBeenAdjusted) return;
             _currentTime += timeAmount;
             if (_currentTime >= _maxTime)
             {
-                Destroy(this);
+                RemoveBuildTimeAdjustment();
             }
         }
         private void OnDisable()
         {
+            if (!_buildTimeHasBeenAdjusted) return;
+            RemoveBuildTimeAdjustment();
+        }
+        private void RemoveBuildTimeAdjustment()
+        {
+            if (_debugBool) Debug.Log($"remove negative build time");
+            _buildTimeHasBeenAdjusted = false;
             AddBuildTimeMax(-_buildTimeToAdd);
         }
         private void AddBuildTimeMax(float timeToAdd)
