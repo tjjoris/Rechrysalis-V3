2f607c2bdd8f5d7d2643aa86d80bbd1e84167d94
tjjoris <tjjoris@gmail.com>
2023-06-15 19:55:41 -0400

    HE increase speed inherits form HE function parent

--- Rechrysalis Another Draft 3/Assets/Units/Hatch Effects/HEIncreaseSpeed.cs
+++ Rechrysalis Another Draft 3/Assets/Units/Hatch Effects/HEIncreaseSpeed.cs
@@ -5,7 +5,7 @@ using Rechrysalis.Unit;
 
 namespace Rechrysalis.HatchEffect
 {
-    public class HEIncreaseSpeed : MonoBehaviour
+    public class HEIncreaseSpeed : HatchEffectFunctionParent
     {
         [SerializeField] private float _speedToAdd;
         [SerializeField] private HatchEffectManager _hatchEffectManager;
