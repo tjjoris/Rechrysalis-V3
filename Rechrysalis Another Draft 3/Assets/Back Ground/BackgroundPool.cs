1bab3b7cc5e695f1a31ce0a6b59a8c99e6525fa4
tjjoris <tjjoris@gmail.com>
2023-06-28 07:58:24 -0400

    refactored map tile removal

--- Rechrysalis Another Draft 3/Assets/Back Ground/BackgroundPool.cs
+++ Rechrysalis Another Draft 3/Assets/Back Ground/BackgroundPool.cs
@@ -25,20 +25,10 @@ namespace Rechrysalis.Background
             this.amountToPool = amountToPool;
             pooledObjects = new List<GameObject>();
             GameObject tmp;
-            // _projectilesHolderGO = GameMaster.Instance.ReferenceManager.ProjectilesHolder;
-            // _projectilesHolderScript = _projectilesHolderGO.GetComponent<ProjectilesHolder>();
             for (int i = 0; i < this.amountToPool; i++)
             {
                 tmp = Instantiate(objectToPool, transform);
-                // ProjectileHandler ph = tmp.GetComponent<ProjectileHandler>();
-                // _projectilesHolderScript.ProjectileHandlers.Add(ph);
-                // ph.Initialize(_projectileSprite);
-                // tmp.GetComponent<SpriteRenderer>().sprite = _projectileSprite;
                 tmp.SetActive(false);
-                // tmp.transform.SetParent(GameMaster.Instance.ReferenceManager.ProjectilesHolder.transform);
-                // ph.ProjectileDuration = _projectileDuration;
-                // ph.Damage = _damage;
-                // ph.ParentUnit = gameObject;
                 pooledObjects.Add(tmp);
             }
         }
@@ -55,30 +45,22 @@ namespace Rechrysalis.Background
         }
         public void Tick ()
         {
-            // Debug.Log($"background tick");
-            if (_activeObjects.Count > 0)
+            LoopToRemoveTiles();
+            for (int _xIndex = 0; _xIndex < _xCount; _xIndex++)
             {
-                for (int _activeIndex = 0; _activeIndex < _activeObjects.Count; _activeIndex++)
-                {
-                    CheckToRemoveTile(_activeObjects[_activeIndex]);
-                }
-            }
-            for (int _xIndex = 0; _xIndex < _xCount; _xIndex ++)
-            {
-                for (int _yIndex = 0; _yIndex < _yCount; _yIndex ++)
+                for (int _yIndex = 0; _yIndex < _yCount; _yIndex++)
                 {
                     bool _objectExists = false;
                     float _xCameraCount = Camera.main.transform.position.x / _tileWidth;
                     _xCameraCount = Mathf.Floor(_xCameraCount);
-                    float _xIndexToCheck = _xCameraCount + _xIndex - ((_xCount -1) * 0.5f);
+                    float _xIndexToCheck = _xCameraCount + _xIndex - ((_xCount - 1) * 0.5f);
                     float _yCameraCount = Camera.main.transform.position.y / _tileHeight;
                     _yCameraCount = Mathf.Floor(_yCameraCount);
-                    float _yIndexToCheck = _yCameraCount + _yIndex - ((_yCount -1) * 0.5f);
+                    float _yIndexToCheck = _yCameraCount + _yIndex - ((_yCount - 1) * 0.5f);
                     Vector3 _vectorToCheck = new Vector3(((_xIndexToCheck * _tileWidth)), (_yIndexToCheck * _tileHeight), _zOffset);
-                    // Debug.Log($"vector " + _vectorToCheck + "x Index " + _xIndex + " x camera count " + _xCameraCount);
                     if (_activeObjects.Count > 0)
                     {
-                        for (int _activeIndex = 0; _activeIndex < _activeObjects.Count; _activeIndex ++)
+                        for (int _activeIndex = 0; _activeIndex < _activeObjects.Count; _activeIndex++)
                         {
                             if (!_objectExists)
                             {
@@ -94,6 +76,9 @@ namespace Rechrysalis.Background
             }
             _verticalBoundsYUpdate.UpdateVerticalBoxColliders(Camera.main.transform);
         }
+
+       
+
         private void ActivateTile (Vector3 _location)
         {
             GameObject go = GetPooledObject();
@@ -110,17 +95,34 @@ namespace Rechrysalis.Background
             }
             else return false;
         }
-        private void CheckToRemoveTile (GameObject _tile)
+        private void LoopToRemoveTiles()
         {
-            if (Mathf.Abs((Camera.main.transform.position.y - _tile.transform.position.y)) > ((_tileHeight * _yCount * 0.6f)))
+            if (_activeObjects.Count == 0) 
+            {return;}
+            for (int _activeIndex = 0; _activeIndex < _activeObjects.Count; _activeIndex++)
             {
-                RemoveTile(_tile);
-            }
-            if (Mathf.Abs((Camera.main.transform.position.x - _tile.transform.position.x)) > ((_tileWidth * _xCount * 0.6f)))
-            {
-                RemoveTile(_tile);
+                CheckToRemoveTile(_activeObjects[_activeIndex]);
             }
         }
+        private void CheckToRemoveTile (GameObject _tile)
+        {
+            TileOutsideXBounds(_tile);
+            TileOutsideYBounds(_tile);
+        }
+
+        private void TileOutsideXBounds(GameObject _tile)
+        {
+            if (!(Mathf.Abs((Camera.main.transform.position.y - _tile.transform.position.y)) > ((_tileHeight * _yCount * 0.6f))))
+            { return; }
+            RemoveTile(_tile);
+        }
+        private void TileOutsideYBounds(GameObject _tile)
+        {
+            if (!(Mathf.Abs((Camera.main.transform.position.x - _tile.transform.position.x)) > ((_tileWidth * _xCount * 0.6f))))
+            {return;}
+            RemoveTile(_tile);            
+        }
+
         private void RemoveTile (GameObject _tile)
         {
 
